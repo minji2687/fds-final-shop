8f34e0b741f864e7a26ad94547ccc1e6ccc04f9d
Kim Seungha <seungha.me@gmail.com>
2018-11-29 16:28:25 +0900

    로그인 기능 추가

--- src/components/LoginFormView.js
+++ src/components/LoginFormView.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
 
 export default class LoginFormView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  handleUsernameChange(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  handleLoginButtonClick() {
+    const { onLogin } = this.props;
+    const { username, password } = this.state;
+    onLogin(username, password);
+  }
+
   render() {
+    const { username, password } = this.state;
     return (
       <div>
-        <input type="text" />
-        <input type="password" />
-        <button>로그인</button>
+        <input
+          type="text"
+          value={username}
+          onChange={e => this.handleUsernameChange(e)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={e => this.handlePasswordChange(e)}
+        />
+        <button onClick={() => this.handleLoginButtonClick()}>로그인</button>
       </div>
     );
   }
