b35f18b55b595ec9572067c077ec5a261d6d1df5
Kim Seungha <seungha.me@gmail.com>
2018-11-30 12:36:28 +0900

    category 필터링: key를 바꿔서 componentDidMount가 다시 호출되도록 수정

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import ProductList from '../containers/ProductList';
 import Layout from '../components/Layout';
+import { Link } from 'react-router-dom';
 
 export default class HomePage extends Component {
   render() {
+    const { location } = this.props;
+    console.log(location.search);
+    const p = new URLSearchParams(location.search);
+    const category = p.get('category');
     return (
       <Layout>
         <h1>Home</h1>
-        <ProductList />
+        <Link to="/">ALL</Link>
+        <Link to="/?category=top">TOP</Link>
+        <Link to="/?category=pants">PANTS</Link>
+        <ProductList key={category} category={category} />
       </Layout>
     );
   }
