4368f75555522e69c5dd314238df87f0a769ae10
Kim Seungha <seungha.me@gmail.com>
2018-11-30 11:21:40 +0900

    key를 사용해 강제로 HeaderView 상태 초기화

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,4 +1,11 @@
+import React from 'react';
 import HeaderView from '../components/HeaderView';
-import { withUser } from '../contexts/UserContext';
+import { withUser, UserConsumer } from '../contexts/UserContext';
 
-export default withUser(HeaderView);
+export default function Header(props) {
+  return (
+    <UserConsumer>
+      {value => <HeaderView key={value.username} {...value} />}
+    </UserConsumer>
+  );
+}
